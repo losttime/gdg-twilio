4335521bb31b7f248b63197c5032728804bf7290
David Cox <losttime.shuffle@gmail.com>
2013-02-08 00:27:35 -0700

    Update code for phase 1

--- phase1/sendSms.py
+++ phase1/sendSms.py
@@ -1,12 +1,13 @@
 from twilio.rest import TwilioRestClient
 
+# To find these visit https://www.twilio.com/user/account
 account = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Twilio Account SID
 token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Twilio Auth Token
 
 fromNum = "+15005550006"  # Twilio number to send SMS from
 toNum = "+15005550009"  # Number to send SMS to (can be non-Twilio number)
 
-message = "Hello"
+message = "GDG"
 
 client = TwilioRestClient(account, token)
 
