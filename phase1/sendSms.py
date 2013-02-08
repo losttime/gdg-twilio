9953e68720792b47f8214a0fce32596319fbebae
David Cox <losttime.shuffle@gmail.com>
2013-02-07 22:52:49 -0700

    Added script for phase 1 task

---
+++ phase1/sendSms.py
@@ -0,0 +1,13 @@
+from twilio.rest import TwilioRestClient
+
+account = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Twilio Account SID
+token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Twilio Auth Token
+
+fromNum = "+15005550006"  # Twilio number to send SMS from
+toNum = "+15005550009"  # Number to send SMS to (can be non-Twilio number)
+
+message = "Hello"
+
+client = TwilioRestClient(account, token)
+
+message = client.sms.messages.create(to=toNum, from_=fromNum, body=message)
